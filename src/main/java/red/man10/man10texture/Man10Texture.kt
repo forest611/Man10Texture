cb93f6ae12c720d1476ac20fa2aa9aa9434490cd
forest611 <forest611328@gmail.com>
2020-05-10 20:52:14 +0900

    ダメージ値見れるように

--- src/main/java/red/man10/man10texture/Man10Texture.kt
+++ src/main/java/red/man10/man10texture/Man10Texture.kt
@@ -2,8 +2,10 @@ package red.man10.man10texture
 
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Damageable
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -11,6 +13,7 @@ import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
 import java.lang.Exception
 
@@ -35,6 +38,7 @@ class Man10Texture : JavaPlugin(),Listener {
             return true
         }
 
+        //手持ちアイテムのcmdチェック
         if (args[0] == "get"){
             try {
                 val id = sender.inventory.itemInMainHand.itemMeta.customModelData
@@ -44,6 +48,12 @@ class Man10Texture : JavaPlugin(),Listener {
                 sender.sendMessage("§a§l0")
             }
             return true
+        }
+
+        if (args[0] == "damage") {
+
+            openDamage(sender,0)
+
         }
 
         return false
@@ -112,6 +122,71 @@ class Man10Texture : JavaPlugin(),Listener {
         val numberData = ItemStack(Material.COMPASS)
         val m = numberData.itemMeta
         m.setDisplayName(number.toString())
+        m.persistentDataContainer.set(NamespacedKey(this,"type"), PersistentDataType.STRING,"damage")
+        numberData.itemMeta = m
+        inv.setItem(49,numberData)
+
+        p.openInventory(inv)
+
+    }
+
+    fun openDamage(p:Player,n:Int){
+
+        var number = n
+
+        if (number <0)number = 0
+
+        val inv = Bukkit.createInventory(null,54,prefix + number)
+
+        for (i in number..number+44){
+            val item = ItemStack(Material.DIAMOND_HOE,1)
+            val meta = item.itemMeta
+            meta.setDisplayName("§a§lDamage = $i")
+            meta.lore = mutableListOf("§a§l${i / 1561}")
+            meta.isUnbreakable = true
+            if (meta is Damageable){
+                (meta as Damageable).damage(i.toDouble())
+            }
+            meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE)
+            meta.addItemFlags(ItemFlag.HIDE_DESTROYS)
+            item.itemMeta = meta
+            inv.addItem(item)
+        }
+        //45 ~53
+        val btn = ItemStack(Material.REDSTONE_BLOCK)
+        var meta = btn.itemMeta
+//        meta.setDisplayName("§c§l-10000")
+//        btn.itemMeta = meta
+//        inv.setItem(45,btn)
+        meta.setDisplayName("§c§l-1000")
+        btn.itemMeta = meta
+        inv.setItem(46,btn)
+        meta.setDisplayName("§c§l-100")
+        btn.itemMeta = meta
+        inv.setItem(47,btn)
+        meta.setDisplayName("§c§l-45")
+        btn.itemMeta = meta
+        inv.setItem(48,btn)
+
+        btn.type = Material.LAPIS_BLOCK
+        meta = btn.itemMeta
+        meta.setDisplayName("§9§l+45")
+        btn.itemMeta = meta
+        inv.setItem(50,btn)
+        meta.setDisplayName("§9§l+100")
+        btn.itemMeta = meta
+        inv.setItem(51,btn)
+        meta.setDisplayName("§9§l+1000")
+        btn.itemMeta = meta
+        inv.setItem(52,btn)
+//        meta.setDisplayName("§9§l+10000")
+//        btn.itemMeta = meta
+//        inv.setItem(53,btn)
+
+        val numberData = ItemStack(Material.COMPASS)
+        val m = numberData.itemMeta
+        m.setDisplayName(number.toString())
+        m.persistentDataContainer.set(NamespacedKey(this,"type"), PersistentDataType.STRING,"damage")
         numberData.itemMeta = m
         inv.setItem(49,numberData)
 
@@ -126,6 +201,8 @@ class Man10Texture : JavaPlugin(),Listener {
 
         if (e.view.title.indexOf(prefix) != 0)return
 
+        if (!p.hasPermission("man10tex.op"))return
+
         e.isCancelled = true
 
         val number = e.inventory.getItem(49)!!.itemMeta.displayName.toInt()
@@ -135,6 +212,24 @@ class Man10Texture : JavaPlugin(),Listener {
         if (e.slot <45){
             if (e.slotType != InventoryType.SlotType.CONTAINER)return
             p.inventory.addItem(e.currentItem)
+            return
+        }
+
+        val type = (e.inventory.getItem(49)?:return).itemMeta.persistentDataContainer[NamespacedKey(this,"type"), PersistentDataType.STRING]!!
+
+        if (type == "damage"){
+            when(e.slot){
+                46-> openDamage(p,number - 1000)
+                47-> openDamage(p,number - 100)
+                48-> openDamage(p,number - 45)
+
+                50-> openDamage(p,number + 45)
+                51-> openDamage(p,number + 100)
+                52-> openDamage(p,number + 1000)
+
+                else -> return
+            }
+
             return
         }
 
