1cd3db2a138e1db91076dd02648fca40dbf64a4e
forest611 <forest611328@gmail.com>
2020-05-10 22:16:53 +0900

    バグ修正

--- src/main/java/red/man10/man10texture/Man10Texture.kt
+++ src/main/java/red/man10/man10texture/Man10Texture.kt
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
@@ -20,6 +21,8 @@ import java.lang.Exception
 class Man10Texture : JavaPlugin(),Listener {
 
     val prefix = "mtex"
+    val mapCmd = HashMap<Player,Int>()
+    val mapDamage = HashMap<Player,Int>()
 
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -29,7 +32,7 @@ class Man10Texture : JavaPlugin(),Listener {
         if (!sender.hasPermission("man10tex.op"))return false
 
         if (args.isEmpty()){
-            openInventory(sender,0,Material.DIAMOND_HOE)
+            openInventory(sender,mapCmd[sender]?:0,Material.DIAMOND_HOE)
             return true
         }
 
@@ -52,7 +55,7 @@ class Man10Texture : JavaPlugin(),Listener {
 
         if (args[0] == "damage") {
 
-            openDamage(sender,0)
+            openDamage(sender, mapDamage[sender]?:0)
 
         }
 
@@ -91,38 +94,38 @@ class Man10Texture : JavaPlugin(),Listener {
         //45 ~53
         val btn = ItemStack(Material.REDSTONE_BLOCK)
         var meta = btn.itemMeta
-        meta.setDisplayName("§c§l-10000")
+        meta.setDisplayName("§c§l-50p")
         btn.itemMeta = meta
         inv.setItem(45,btn)
-        meta.setDisplayName("§c§l-1000")
+        meta.setDisplayName("§c§l-10p")
         btn.itemMeta = meta
         inv.setItem(46,btn)
-        meta.setDisplayName("§c§l-100")
+        meta.setDisplayName("§c§l-5p")
         btn.itemMeta = meta
         inv.setItem(47,btn)
-        meta.setDisplayName("§c§l-45")
+        meta.setDisplayName("§c§l-1p")
         btn.itemMeta = meta
         inv.setItem(48,btn)
 
         btn.type = Material.LAPIS_BLOCK
         meta = btn.itemMeta
-        meta.setDisplayName("§9§l+45")
+        meta.setDisplayName("§9§l+1p")
         btn.itemMeta = meta
         inv.setItem(50,btn)
-        meta.setDisplayName("§9§l+100")
+        meta.setDisplayName("§9§l+5p")
         btn.itemMeta = meta
         inv.setItem(51,btn)
-        meta.setDisplayName("§9§l+1000")
+        meta.setDisplayName("§9§l+10p")
         btn.itemMeta = meta
         inv.setItem(52,btn)
-        meta.setDisplayName("§9§l+10000")
+        meta.setDisplayName("§9§l+50p")
         btn.itemMeta = meta
         inv.setItem(53,btn)
 
         val numberData = ItemStack(Material.COMPASS)
         val m = numberData.itemMeta
         m.setDisplayName(number.toString())
-        m.persistentDataContainer.set(NamespacedKey(this,"type"), PersistentDataType.STRING,"damage")
+        m.persistentDataContainer.set(NamespacedKey(this,"type"), PersistentDataType.STRING,"cmd")
         numberData.itemMeta = m
         inv.setItem(49,numberData)
 
@@ -135,18 +138,21 @@ class Man10Texture : JavaPlugin(),Listener {
         var number = n
 
         if (number <0)number = 0
+        if (number >1530)number = 1530
 
         val inv = Bukkit.createInventory(null,54,prefix + number)
 
         for (i in number..number+44){
-            val item = ItemStack(Material.DIAMOND_HOE,1)
+
+            if (i >= 1562)break
+            val item = ItemStack(Material.DIAMOND_HOE,1,i.toShort())
             val meta = item.itemMeta
             meta.setDisplayName("§a§lDamage = $i")
-            meta.lore = mutableListOf("§a§l${i / 1561}")
+            meta.lore = mutableListOf("§a§l${i.toDouble() / 1562.0}")
             meta.isUnbreakable = true
-            if (meta is Damageable){
-                (meta as Damageable).damage(i.toDouble())
-            }
+//            if (meta is Damageable){
+//                (meta as Damageable).damage(i.toDouble())
+//            }
             meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE)
             meta.addItemFlags(ItemFlag.HIDE_DESTROYS)
             item.itemMeta = meta
@@ -155,31 +161,32 @@ class Man10Texture : JavaPlugin(),Listener {
         //45 ~53
         val btn = ItemStack(Material.REDSTONE_BLOCK)
         var meta = btn.itemMeta
-//        meta.setDisplayName("§c§l-10000")
+//        meta.setDisplayName("§c§l-50p")
 //        btn.itemMeta = meta
 //        inv.setItem(45,btn)
-        meta.setDisplayName("§c§l-1000")
+        meta.setDisplayName("§c§l-10p")
         btn.itemMeta = meta
         inv.setItem(46,btn)
-        meta.setDisplayName("§c§l-100")
+        meta.setDisplayName("§c§l-5p")
         btn.itemMeta = meta
         inv.setItem(47,btn)
-        meta.setDisplayName("§c§l-45")
+        meta.setDisplayName("§c§l-1p")
         btn.itemMeta = meta
         inv.setItem(48,btn)
 
+
         btn.type = Material.LAPIS_BLOCK
         meta = btn.itemMeta
-        meta.setDisplayName("§9§l+45")
+        meta.setDisplayName("§9§l+1p")
         btn.itemMeta = meta
         inv.setItem(50,btn)
-        meta.setDisplayName("§9§l+100")
+        meta.setDisplayName("§9§l+5p")
         btn.itemMeta = meta
         inv.setItem(51,btn)
-        meta.setDisplayName("§9§l+1000")
+        meta.setDisplayName("§9§l+10p")
         btn.itemMeta = meta
         inv.setItem(52,btn)
-//        meta.setDisplayName("§9§l+10000")
+//        meta.setDisplayName("§9§l+50p")
 //        btn.itemMeta = meta
 //        inv.setItem(53,btn)
 
@@ -219,13 +226,13 @@ class Man10Texture : JavaPlugin(),Listener {
 
         if (type == "damage"){
             when(e.slot){
-                46-> openDamage(p,number - 1000)
-                47-> openDamage(p,number - 100)
+                46-> openDamage(p,number - 450)
+                47-> openDamage(p,number - 225)
                 48-> openDamage(p,number - 45)
 
                 50-> openDamage(p,number + 45)
-                51-> openDamage(p,number + 100)
-                52-> openDamage(p,number + 1000)
+                51-> openDamage(p,number + 225)
+                52-> openDamage(p,number + 450)
 
                 else -> return
             }
@@ -234,18 +241,38 @@ class Man10Texture : JavaPlugin(),Listener {
         }
 
         when(e.slot){
-            45-> openInventory(p,number - 10000,item.type)
-            46-> openInventory(p,number - 1000,item.type)
-            47-> openInventory(p,number - 100,item.type)
+            45-> openInventory(p,number - 2250,item.type)
+            46-> openInventory(p,number - 450,item.type)
+            47-> openInventory(p,number - 225,item.type)
             48-> openInventory(p,number - 45,item.type)
 
             50-> openInventory(p,number + 45,item.type)
-            51-> openInventory(p,number + 100,item.type)
-            52-> openInventory(p,number + 1000,item.type)
-            53-> openInventory(p,number + 10000,item.type)
+            51-> openInventory(p,number + 225,item.type)
+            52-> openInventory(p,number + 450,item.type)
+            53-> openInventory(p,number + 2250,item.type)
 
             else -> return
         }
+
+    }
+
+    @EventHandler
+    fun close(e:InventoryCloseEvent){
+
+        val p = e.player as Player
+
+        if (e.view.title.indexOf(prefix) != 0)return
+
+        if (!p.hasPermission("man10tex.op"))return
+
+        val number = e.inventory.getItem(49)!!.itemMeta.displayName.toInt()
+
+        if ((e.inventory.getItem(49)?:return).itemMeta.persistentDataContainer[NamespacedKey(this,"type"), PersistentDataType.STRING]!! == "damage"){
+            mapDamage[p] = number
+            return
+        }
+
+        mapCmd[p] = number
 
     }
 }
